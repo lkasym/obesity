d8780b39bf744aebfdd5dcd9da5e8f7c9e193e3e
lkasym <112753425+lkasym@users.noreply.github.com>
2023-10-25 02:44:11 +0530

    Update app_het.py

--- app_het.py
+++ app_het.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import pickle
 from joblib import load
 
 # Load pre-trained models
@@ -17,29 +16,7 @@ model_files = {
 models = {}
 for model_name, model_file in model_files.items():
     with open(model_file, 'rb') as f:
-        models[model_name] = load(f'{model_name}.pkl')
-
-# Define a list of feature names in the correct order
-feature_names = [
-    'Age', 'Gender', 'family_history_with_overweight', 'FAVC', 'FCVC', 'NCP', 'CAEC', 'SMOKE',
-    'CH2O', 'SCC', 'FAF', 'TUE', 'CALC', 'MTRANS', 'BMI'
-]
-
-# Create a dictionary for mapping user inputs to numerical values
-input_mapping = {
-    'Gender': {'Male': 0, 'Female': 1},
-    'family_history_with_overweight': {'Yes': 1, 'No': 0},
-    'FAVC': {'Yes': 1, 'No': 0},
-    'FCVC': {'Never': 0, 'Sometimes': 1, 'Always': 2},
-    'NCP': {'1 meal': 0, '2 meals': 1, '3 meals': 2, '4 meals': 3},
-    'CAEC': {'No': 0, 'Sometimes': 1, 'Frequently': 2, 'Always': 3},
-    'SMOKE': {'Yes': 1, 'No': 0},
-    'CH2O': {'Less than a liter': 0, 'Between 1 and 2 liters': 1, 'More than 2 liters': 2},
-    'SCC': {'Yes': 1, 'No': 0},
-    'FAF': {'None': 0, '1-2 days a week': 1, '3-4 days a week': 2, '4-5 days a week': 3},
-    'TUE': {'0-2 hours': 0, '3-5 hours': 1, 'More than 5 hours': 2},
-    'CALC': {'No': 0, 'Sometimes': 1, 'Frequently': 2, 'Always': 3}
-}
+        models[model_name] = load(f)
 
 # Streamlit interface
 st.title("Obesity Prediction")
@@ -50,31 +27,72 @@ height = st.number_input("Height (in meters)", min_value=1.0, max_value=2.5)
 weight = st.number_input("Weight (in kgs)", min_value=30, max_value=200)
 bmi = weight / (height ** 2)
 
-# Collect user input for Gender
+# Encoding and collecting user input
 gender = st.selectbox("Gender", ["Male", "Female"])
+gender_encoded = 0 if gender == 'Male' else 1
 
-# Collect user input for MTRANS
-mtrans = st.selectbox("MTRANS", ["Public_Transportation", "Walking", "Automobile", "Motorbike", "Bike"])
+family_history_with_overweight = st.selectbox("Family history with overweight?", ["Yes", "No"])
+family_history_with_overweight_encoded = 1 if family_history_with_overweight == 'Yes' else 0
 
-input_data = {
-    'Gender': gender,
+FAVC = st.selectbox("Frequent consumption of high caloric food?", ["Yes", "No"])
+FAVC_encoded = 1 if FAVC == 'Yes' else 0
+
+FCVC = st.selectbox("Frequency of vegetable consumption", ["Never", "Sometimes", "Always"])
+FCVC_encoded = {"Never": 0, "Sometimes": 1, "Always": 2}[FCVC]
+
+NCP = st.selectbox("Number of main meals a day", ["1 meal", "2 meals", "3 meals", "4 meals"])
+NCP_encoded = {"1 meal": 0, "2 meals": 1, "3 meals": 2, "4 meals": 3}[NCP]
+
+CAEC = st.selectbox("Consumption of food between meals", ["No", "Sometimes", "Frequently", "Always"])
+CAEC_encoded = {"No": 0, "Sometimes": 1, "Frequently": 2, "Always": 3}[CAEC]
+
+SMOKE = st.selectbox("Do you smoke?", ["Yes", "No"])
+SMOKE_encoded = 1 if SMOKE == 'Yes' else 0
+
+CH2O = st.selectbox("Daily water consumption", ["Less than a liter", "Between 1 and 2 liters", "More than 2 liters"])
+CH2O_encoded = {"Less than a liter": 0, "Between 1 and 2 liters": 1, "More than 2 liters": 2}[CH2O]
+
+SCC = st.selectbox("Do you monitor calorie consumption?", ["Yes", "No"])
+SCC_encoded = 1 if SCC == 'Yes' else 0
+
+FAF = st.selectbox("Frequency of physical activity", ["None", "1-2 days a week", "3-4 days a week", "4-5 days a week"])
+FAF_encoded = {"None": 0, "1-2 days a week": 1, "3-4 days a week": 2, "4-5 days a week": 3}[FAF]
+
+TUE = st.selectbox("Time using technology devices daily", ["0-2 hours", "3-5 hours", "More than 5 hours"])
+TUE_encoded = {"0-2 hours": 0, "3-5 hours": 1, "More than 5 hours": 2}[TUE]
+
+CALC = st.selectbox("Frequency of alcohol consumption", ["No", "Sometimes", "Frequently", "Always"])
+CALC_encoded = {"No": 0, "Sometimes": 1, "Frequently": 2, "Always": 3}[CALC]
+
+MTRANS = st.selectbox("MTRANS", ["Public_Transportation", "Walking", "Automobile", "Motorbike", "Bike"])
+MTRANS_encoded = {
+    "Public_Transportation": 0,
+    "Walking": 1,
+    "Automobile": 2,
+    "Motorbike": 3,
+    "Bike": 4
+}[MTRANS]
+
+# Construct the encoded and preprocessed input data
+input_data_encoded = {
     'Age': age,
-    'family_history_with_overweight': st.selectbox("Family history with overweight?", ["Yes", "No"]),
-    'FAVC': st.selectbox("Frequent consumption of high caloric food?", ["Yes", "No"]),
-    'FCVC': st.selectbox("Frequency of vegetable consumption", ["Never", "Sometimes", "Always"]),
-    'NCP': st.selectbox("Number of main meals a day", ["1 meal", "2 meals", "3 meals", "4 meals"]),
-    'CAEC': st.selectbox("Consumption of food between meals", ["No", "Sometimes", "Frequently", "Always"]),
-    'SMOKE': st.selectbox("Do you smoke?", ["Yes", "No"]),
-    'CH2O': st.selectbox("Daily water consumption", ["Less than a liter", "Between 1 and 2 liters", "More than 2 liters"]),
-    'SCC': st.selectbox("Do you monitor calorie consumption?", ["Yes", "No"]),
-    'FAF': st.selectbox("Frequency of physical activity", ["None", "1-2 days a week", "3-4 days a week", "4-5 days a week"]),
-    'TUE': st.selectbox("Time using technology devices daily", ["0-2 hours", "3-5 hours", "More than 5 hours"]),
-    'CALC': st.selectbox("Frequency of alcohol consumption", ["No", "Sometimes", "Frequently", "Always"]),
-    'MTRANS': mtrans,
+    'Gender': gender_encoded,
+    'family_history_with_overweight': family_history_with_overweight_encoded,
+    'FAVC': FAVC_encoded,
+    'FCVC': FCVC_encoded,
+    'NCP': NCP_encoded,
+    'CAEC': CAEC_encoded,
+    'SMOKE': SMOKE_encoded,
+    'CH2O': CH2O_encoded,
+    'SCC': SCC_encoded,
+    'FAF': FAF_encoded,
+    'TUE': TUE_encoded,
+    'CALC': CALC_encoded,
+    'MTRANS': MTRANS_encoded,
     'BMI': bmi
 }
 
-input_df = pd.DataFrame([input_data])
+input_df = pd.DataFrame([input_data_encoded])
 
 # Model selection
 selected_model = st.selectbox(
@@ -87,4 +105,3 @@ if st.button("Predict"):
     model = models[selected_model]
     prediction = model.predict(input_df)
     st.write(f"Predicted Class: {prediction[0]}")
-
